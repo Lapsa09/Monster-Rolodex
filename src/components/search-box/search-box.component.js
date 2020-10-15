fd449526e00e0c9aa5f580a7e7fdf790e6cb0e3d
Lapsa <agusdinar@hotmail.com>
2020-10-15 17:46:57 -0300

    Adding files to gh-page

---
+++ src/components/search-box/search-box.component.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./search-box.styles.css";
+
+export function SearchBox({ placeholder, handleChange }) {
+  return (
+    <div>
+      <input
+        className="search"
+        type="search"
+        placeholder={placeholder}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
